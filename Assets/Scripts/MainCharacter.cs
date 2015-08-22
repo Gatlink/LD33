95d2ce319cd22d736c542849a732002a78a3bc2d
Florian BOEUF-TERRU <boeuf.florian@gmail.com>
2015-08-22 13:41:30 +0200

    Gamepad

--- Assets/Scripts/MainCharacter.cs
+++ Assets/Scripts/MainCharacter.cs
@@ -1,50 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MainCharacter : Movable {
+public class MainCharacter : Movable 
+{
 	public float Speed = 10f;
 	[Range(0f,1f)]
 	public float Friction = 1f;
 
 	public override void Update () 
-	{
-		base.Update();
+	{		
 		ProcessInput();
+		base.Update();
 	}
 
 	private void ProcessInput() 
 	{
 		Vector3 axis = Vector3.zero;
 		bool move = false;
+		float speed = Speed;
 
-		if(Input.GetKey(KeyCode.UpArrow)) 
+		if(Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f)
 		{
-			axis += Vector3.up;
+			Debug.Log("meh");
 			move = true;
+			axis.x = Input.GetAxis("Horizontal");
+			axis.y = Input.GetAxis("Vertical");
+			speed *= Mathf.Max(Mathf.Abs(axis.x), Mathf.Abs(axis.y));
+			Debug.Log(speed);
 		}
-
-		if(Input.GetKey(KeyCode.DownArrow)) 
+		else 
 		{
-			axis += Vector3.down;
-			move = true;
-		}
+			if(Input.GetKey(KeyCode.UpArrow)) 
+			{
+				axis += Vector3.up;
+				move = true;
+			}
 
-		if(Input.GetKey(KeyCode.LeftArrow)) 
-		{
-			axis += Vector3.left;
-			move = true;
-		}
+			if(Input.GetKey(KeyCode.DownArrow)) 
+			{
+				axis += Vector3.down;
+				move = true;
+			}
 
-		if(Input.GetKey(KeyCode.RightArrow)) 
-		{
-			axis += Vector3.right;
-			move = true;
+			if(Input.GetKey(KeyCode.LeftArrow)) 
+			{
+				axis += Vector3.left;
+				move = true;
+			}
+
+			if(Input.GetKey(KeyCode.RightArrow)) 
+			{
+				axis += Vector3.right;
+				move = true;
+			}
 		}
 
 		if(move)
 		{
-			axis.Normalize();
-			Move(axis, Speed, Friction);
+			Move(axis, speed, Friction);
 		}
 	}
 }
