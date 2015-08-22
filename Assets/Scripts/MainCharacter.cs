0101f981b9a760458fa3d3d91986e96a92d8ec25
Florian BOEUF-TERRU <boeuf.florian@gmail.com>
2015-08-22 12:40:34 +0200

    Movable component + MainCHaracter Direction avec touches fléchées

---
+++ Assets/Scripts/MainCharacter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCharacter : Movable {
+	public float Speed = 10f;
+	[Range(0f,1f)]
+	public float Friction = 1f;
+
+	public override void Update () 
+	{
+		base.Update();
+		ProcessInput();
+	}
+
+	private void ProcessInput() 
+	{
+		Vector3 axis = Vector3.zero;
+		bool move = false;
+
+		if(Input.GetKey(KeyCode.UpArrow)) 
+		{
+			axis += Vector3.up;
+			move = true;
+		}
+
+		if(Input.GetKey(KeyCode.DownArrow)) 
+		{
+			axis += Vector3.down;
+			move = true;
+		}
+
+		if(Input.GetKey(KeyCode.LeftArrow)) 
+		{
+			axis += Vector3.left;
+			move = true;
+		}
+
+		if(Input.GetKey(KeyCode.RightArrow)) 
+		{
+			axis += Vector3.right;
+			move = true;
+		}
+
+		if(move)
+		{
+			axis.Normalize();
+			Move(axis, Speed, Friction);
+		}
+	}
+}
