d645ce6ca795aca2a3fbd2324c1ca35237cb35a1
Florian BOEUF-TERRU <boeuf.florian@gmail.com>
2015-08-22 23:15:07 +0200

    follow cam cooldown entre les attaques du squelette

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,31 @@ using System.Collections;
 public abstract class Enemy : Character {
 	public float AcquisitionRange = 150f;
 
+	public float AttackCooldown = 1f;
+
 	[Space(5)]
 	[Range(0f,5f)]
 	public float MinRandomDelay = 1f;
 	[Range(1f,10f)]
 	public float MaxRandomDelay = 3f;
 
+	private Coroutine _randomMovement;
+
 	public bool Tracking 
 	{
 		get; protected set;
+	}
+
+	public bool CanAttack
+	{
+		get; protected set;
+	}
+
+	public override void Awake()
+	{
+		base.Awake();
+		CanAttack = true;
+		_randomMovement = null;
 	}
 
 	public override void Update()
@@ -24,7 +40,11 @@ public abstract class Enemy : Character {
 		if(distFromHero <= AcquisitionRange * AcquisitionRange || Tracking) 
 		{
 			Acquire();
-			StopAllCoroutines();
+			if(_randomMovement != null)
+			{
+				StopCoroutine(_randomMovement);
+				_randomMovement = null;
+			}
 		}
 		else if(!Moving)
 		{
@@ -41,10 +61,10 @@ public abstract class Enemy : Character {
 		float duration = Random.Range(MinRandomDelay, MaxRandomDelay);
 		Vector3 dir = Random.insideUnitCircle;
 		dir.Normalize();
-		StartCoroutine(KeepMoving(duration, dir));
+		_randomMovement = StartCoroutine(KeepMoving(duration, dir));
 	}
 
-	private IEnumerator KeepMoving(float duration, Vector3 direction) 
+	protected IEnumerator KeepMoving(float duration, Vector3 direction) 
 	{
 		float start = Time.time;
 		float elapsed = 0f;
@@ -53,5 +73,16 @@ public abstract class Enemy : Character {
 			Move(direction);
 			yield return 0;
 		}
+	}
+
+	protected IEnumerator CoolWeaponDown() 
+	{
+		float start = Time.time;
+		float elapsed = 0f;
+		while(elapsed < AttackCooldown) {
+			elapsed = Time.time - start;
+			yield return 0;
+		}
+		CanAttack = true;
 	}
 }
